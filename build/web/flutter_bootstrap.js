5d1982533adc266f0fc7797954356d23298f94ee
WolfSlayer04 <133533095+WolfSlayer04@users.noreply.github.com>
2025-02-17 21:50:50 -0600

    web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3648773915"
+    serviceWorkerVersion: "2556946352"
   }
 });
